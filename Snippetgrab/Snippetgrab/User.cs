e0965891ce175511e8f204e7bd724c8889c4a510
Meijer <max1_meijer@hotmail.com>
2016-10-07 13:27:54 +0200

    User class added

---
+++ Snippetgrab/Snippetgrab/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snippetgrab
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime JoinDate { get; set; }
+        public int Reputation { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public User(int id, string name, DateTime joindate, int reputation, string email, bool IsAdmin)
+        {
+            ID = id;
+            Name = name;
+            JoinDate = joindate;
+        }
+    }
+}
